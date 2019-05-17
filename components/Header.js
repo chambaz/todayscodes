277a253cdebea26ac20b6b6dddbb8fabfff94e28
Adam Chambers <adammchambers@gmail.com>
2019-05-17 01:47:19 -0400

    💄 Add pink color to logo

--- components/Header.js
+++ components/Header.js
@@ -17,11 +17,13 @@ const Logo = styled.a({
   textDecoration: 'none'
 })
 
-const GitHubButton = styled.a({
+const GitHubButton = styled.button({
   background: '#333',
   border: 'solid 2px #333',
   borderRadius: 5,
   color: '#fff',
+  fontFamily: 'Menlo',
+  fontSize: '16px',
   padding: '15px 20px',
   textDecoration: 'none',
   transition: '.3s',
@@ -36,7 +38,9 @@ function Header() {
   return (
     <Container>
       <LogoContainer>
-        <Logo href="/">todays.codes</Logo>
+        <Logo href="/">
+          <span style={{ color: '#c3367c' }}>todays</span>.codes
+        </Logo>
       </LogoContainer>
       <GitHubButton href="#">
         <i class="fab fa-github fa-lg" style={{ marginRight: 15 }} />
