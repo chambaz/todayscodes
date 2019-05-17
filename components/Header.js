1ab358a57b341f39b4b1e98ce8a77c06dbb52990
Adam Chambers <adammchambers@gmail.com>
2019-05-17 08:44:59 -0400

    🚧 Basic Firebase GitHub authentication

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,8 @@
+import { useState } from 'react'
 import styled from '@emotion/styled'
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
 import '@fortawesome/fontawesome-free/js/all'
 
 const Container = styled.header({
@@ -36,6 +40,44 @@ const GitHubButton = styled.button({
 })
 
 function Header() {
+  const [user, setUser] = useState({})
+  const provider = new firebase.auth.GithubAuthProvider()
+  provider.addScope('repo')
+
+  if (!firebase.apps.length) {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyCbO50pwHrdC8ptrjxaR_OqqOfYQDm8BM8',
+      authDomain: 'todayscodes-49059.firebaseapp.com',
+      databaseURL: 'https://todayscodes-49059.firebaseio.com',
+      projectId: 'todayscodes-49059',
+      storageBucket: 'todayscodes-49059.appspot.com',
+      messagingSenderId: '1015784084598',
+      appId: '1:1015784084598:web:ca8d3bf3016d8f5d'
+    })
+
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        setUser(user)
+      }
+    })
+  }
+
+  function signIn() {
+    if (!user.email) {
+      firebase
+        .auth()
+        .signInWithPopup(provider)
+        .then(result => {
+          setUser(result.user)
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    } else {
+      alert('Redirect to commit submit page!')
+    }
+  }
+
   return (
     <Container>
       <LogoContainer>
@@ -43,8 +85,8 @@ function Header() {
           <span style={{ color: '#c3367c' }}>todays</span>.codes
         </Logo>
       </LogoContainer>
-      <GitHubButton href="#">
-        <i class="fab fa-github fa-lg" style={{ marginRight: 15 }} />
+      <GitHubButton onClick={signIn}>
+        <i className="fab fa-github fa-lg" style={{ marginRight: 15 }} />
         Share your commit
       </GitHubButton>
     </Container>
