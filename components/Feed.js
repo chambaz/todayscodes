e0664be7f9b68e4abca58970698cf67ba2d82fd0
Adam Chambers <adammchambers@gmail.com>
2019-05-17 01:47:33 -0400

    💄 Load more button in feed component

--- components/Feed.js
+++ components/Feed.js
@@ -5,12 +5,36 @@ const Container = styled.div({
   paddingTop: 30
 })
 
+const LoadMore = styled.button({
+  background: '#c3367c',
+  border: 'solid 2px #c3367c',
+  borderRadius: 5,
+  color: '#fff',
+  display: 'block',
+  fontFamily: 'Menlo',
+  fontSize: '16px',
+  margin: '40px auto',
+  padding: '15px 20px',
+  textDecoration: 'none',
+  transition: '.3s',
+
+  ':hover': {
+    background: '#fff',
+    color: '#c3367c'
+  }
+})
+
 function Feed() {
   return (
     <Container>
       <Commit />
       <Commit />
       <Commit />
+
+      <LoadMore>
+        <i class="fas fa-plus" style={{ marginRight: 15 }} />
+        Checkout yesterday's commits
+      </LoadMore>
     </Container>
   )
 }
