c5d7f8f1031f9e57e09f42b57750bd87227fb885
Adam Chambers <adammchambers@gmail.com>
2019-05-25 14:36:03 -0400

    🚧 Submit page component

---
+++ pages/submit.js
@@ -0,0 +1,12 @@
+import App from '../components/App'
+import Header from '../components/Header'
+
+function Submit() {
+  return (
+    <App>
+      <Header />
+    </App>
+  )
+}
+
+export default Submit
